4599f1c48c98ad3d097c0a82df6c0e8a3b658455
junaids79 <junaids5954@gmail.com>
2026-07-03 22:36:09 +0530

    version update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Projects from './components/Projects'
 import Education from './components/Education'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
